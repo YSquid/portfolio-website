af6e3890711409859dc78c079805e8b181ee897b
Ysquid <ahk648@gmail.com>
2022-09-26 15:52:05 -0600

    Middle of writing JS for details

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-document.querySelector('#mybutton').onclick = function() {
-    const title = document.querySelector('#test-title');
-    title.style.color = red;
+/*document.getElementById('mybutton').onclick = function() {
+    const h5 = document.getElementById('h5')
+    h5.style.color = 'red';
+}*/
+
+/*document.getElementById('details-button-1').onclick = function() {
+    const details = document.querySelector('#cheat-sheet-details')
+    details.style.display = 'block'
+}*/
+
+/*document.getElementById('cs-detail-toggle').onclick = function() {
+    document.getElementById('cheat-sheet-details').style.display = 'block'
 }
+*/
+
+const toggle = document.getElementById('cs-detail-toggle')
+const csDetail = document.getElementById('cheat-sheet-details')
+
+function toggleDisplay(obj) {
+    csDetail.style.display = 'block'
+}
+
+toggle.onclick = toggleDisplay
+
+
